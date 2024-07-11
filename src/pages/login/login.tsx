89d8261f7a52ffd56667600c90c3bdbe79550331
Marwin Jay <95360730+pitohui02@users.noreply.github.com>
2024-07-11 21:40:40 +0000

    refactored: login page; done

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,35 +1,44 @@
 import { Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
+import { Button } from "../../components/ui/button";
+import { Checkbox } from "../../components/ui/checkbox";
+
 
 
 export default function LoginPage() {
     return (
        <>
         <div className="bg-signupBg grid place-items-center h-screen no-repeat bg-cover">
-            <div className="flex-col justify-items-center bg-white min-w-[582px] min-h-[776px] rounded-md p-8">
-                <h1 className="uppercase text-[35px] text-center font-mono font-medium">onlyimages</h1>
-                    
-                    <form className="grid gap-y-4 justify-items-center items-center w-auto">
+            <div className="w-full h-screen flex justify-center items-center bg-black bg-opacity-50">
+                <div className="flex flex-col justify-center items-center space-y-6 bg-white min-w-[450px] min-h-[450px] rounded-[24px] p-8">
+                    <h1 className="uppercase text-[35px] text-center font-mono font-medium">onlyimages</h1>
                         
-                        <div className="flex-row justify-items-start">
-                            <Label htmlFor="loginEmail" className= "text-lg">Username</Label>
-                            <Input id="loginUsername" placeholder="Username" className="rounded-full min-w-[300px]"/>
+                        <form className="flex flex-col space-y-4 w-fit">
+                            
+                            <div className="flex-row justify-items-start">
+                                <Label htmlFor="loginEmail" className= "text-lg">Username</Label>
+                                <Input id="loginUsername" placeholder="Username" className="rounded-full min-w-[300px]"/>
+                            </div>
+
+                            <div className="flex-row justify-items-start">
+                                <Label htmlFor="passwordEmail" className= "text-lg">Password</Label>
+                                <Input id="passwordEmail" type="password" placeholder="Password" className="rounded-full min-w-[300px]"/>
+                            </div>
+
+                            <div className="flex flex-row space-x-1.5 items-center">
+                                <Checkbox id="rememberMe"/>
+                                <Label htmlFor="rememberMe">Remember Me</Label>
+                            </div>
+
+                            <Button variant="outline" className="bg-[#FFAF8A] min-w-[300px] rounded-lg hover:bg-[#F88D5B] duration-500 ">Login</Button>
+                        </form>
+
+                        <div className="flex flex-col space-y-2 items-center mt-[20px]">
+                            <Link to="/forgot_password" className="text-sm">Forgot Password</Link>
+                            <Link to="/register" className="text-sm text-[#FFAF8A] underline">Create a New Account</Link>
                         </div>
-
-                        <div className="flex-row justify-items-start">
-                            <Label htmlFor="passwordEmail" className= "text-lg">Password</Label>
-                            <Input id="passwordEmail" type="password" placeholder="Password" className="rounded-full min-w-[300px]"/>
-                        </div>
-
-                        <Button variant="outline" className="bg-[#FFAF8A] min-w-[300px] rounded-full hover:bg-[#F88D5B] ">Login</Button>
-                    </form>
-
-                    
-                    
-
-                <div className=""></div>
+                </div>
             </div>
 
 
