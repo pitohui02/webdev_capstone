73e49d8156b9d8ab22280f1eec4cb7399d6af047
Marwin Jay <95360730+pitohui02@users.noreply.github.com>
2024-07-12 16:32:59 +0000

    made changes to shadcn file location

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
-import { Input } from "../../components/ui/input";
-import { Label } from "../../components/ui/label";
-import { Button } from "../../components/ui/button";
-import { Checkbox } from "../../components/ui/checkbox";
+import { Input } from "../../@/components/ui/input";
+import { Label } from "../../@/components/ui/label";
+import { Button } from "../../@/components/ui//button";
+import { Checkbox } from "../../@/components/ui/checkbox";
 
 
 
