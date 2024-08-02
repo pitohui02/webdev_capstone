c88145ca7dc41f407b2cd6a7e08a6aca0a58f8b0
Nicoco <91542808+OdeeyuhLog@users.noreply.github.com>
2024-08-02 21:06:24 +0800

    Revert old forgot password modal

--- src/components/ForgotPasswordModal.tsx
+++ src/components/ForgotPasswordModal.tsx
@@ -1,157 +1,132 @@
 import { Button } from "../@/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "../@/components/ui/dialog";
 import { Input } from "../@/components/ui/input";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
-    Form,
-    FormControl,
-    FormField,
-    FormItem,
-    FormLabel,
-    FormMessage,
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
 } from "../@/components/ui/form";
+import { useMutation } from "@tanstack/react-query";
+import { supabase } from "../client";
+import { useToast } from "../@/components/ui/use-toast";
+import { ReloadIcon } from "@radix-ui/react-icons";
 
-const FormSchema = z
-    .object({
-        email: z.string().min(1, "Email is required").email("Invalid email"),
-        newPassword: z.string().min(6, "Password must be at least 6 characters long"),
-        confirmPassword: z.string().min(6, "Confirm password must be at least 6 characters long"),
-    })
-    .refine((data) => data.newPassword === data.confirmPassword, {
-        message: "Passwords don't match",
-        path: ["confirmPassword"],
-    });
+const FormSchema = z.object({
+	email: z.string().min(1, "Email is required").email("Invalid email"),
+});
 
 const ForgotPasswordModal = () => {
-    const form = useForm<z.infer<typeof FormSchema>>({
-        resolver: zodResolver(FormSchema),
-        defaultValues: {
-            email: "",
-            newPassword: "",
-            confirmPassword: "",
-        },
-    });
+	const { toast } = useToast();
 
-    const onSubmit = async (values: z.infer<typeof FormSchema>) => {
-        try {
-            const response = await fetch('/api/change-password', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ email: values.email, newPassword: values.newPassword }),
-            });
+	const form = useForm<z.infer<typeof FormSchema>>({
+		resolver: zodResolver(FormSchema),
+		defaultValues: {
+			email: "",
+		},
+	});
 
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw new Error(errorData.message || 'Network response was not ok');
-            }
+	const mutation = useMutation({
+		mutationFn: async (values: z.infer<typeof FormSchema>) => {
+			const { data, error } = await supabase.auth.resetPasswordForEmail(
+				values.email,
+			);
+			if (error) throw error;
+			return data;
+		},
+		onSuccess: () => {
+			toast({
+				title: "Reset password email sent",
+				description: "Please check your email",
+			});
+		},
+		onError: () => {
+			toast({
+				title: "Error in sending email",
+				description: "Email has not been sent",
+				variant: "destructive",
+			});
+		},
+	});
 
-            alert('Password successfully changed.');
-        } catch (error) {
-            console.error('Failed to change password:', error);
-            alert('Failed to change password. Please try again.');
-        }
-    };
+	const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+		mutation.mutate(values);
+	};
 
-    return (
-        <>
-            <Dialog>
-                <DialogTrigger asChild>
-                    <Button variant="ghost" className="text-sm">
-                        Forgot Password
-                    </Button>
-                </DialogTrigger>
+	return (
+		<>
+			<Dialog>
+				<DialogTrigger asChild>
+					<Button variant="ghost" className="text-sm">
+						Forgot Password
+					</Button>
+				</DialogTrigger>
 
-                <DialogContent className="sm:max-w-[680px]">
-                    <DialogHeader className="flex flex-col items-center">
-                        <DialogTitle>Forgot Password</DialogTitle>
-                        <DialogDescription>
-                            You can reset your password by entering your email and a new password.
-                        </DialogDescription>
-                    </DialogHeader>
+				<DialogContent className="sm:max-w-[680px]">
+					<DialogHeader className="flex flex-col items-center">
+						<DialogTitle>Forgot Password</DialogTitle>
+						<DialogDescription>
+							You can reset your password by entering your email and a new
+							password.
+						</DialogDescription>
+					</DialogHeader>
 
-                    <Form {...form}>
-                        <form
-                            onSubmit={form.handleSubmit(onSubmit)}
-                            className="flex flex-col space-y-4 items-center"
-                        >
-                            <FormField
-                                control={form.control}
-                                name="email"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="text-sm">Email</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="johndoe@email.com"
-                                                type="email"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
+					<Form {...form}>
+						<form
+							onSubmit={form.handleSubmit(onSubmit)}
+							className="flex flex-row space-x-2 items-end justify-center"
+						>
+							<FormField
+								control={form.control}
+								name="email"
+								render={({ field }) => (
+									<FormItem>
+										<FormLabel className="text-sm">Email</FormLabel>
+										<FormControl>
+											<Input
+												placeholder="johndoe@email.com"
+												type="email"
+												{...field}
+											/>
+										</FormControl>
+										<FormMessage />
+									</FormItem>
+								)}
+							/>
 
-                            <FormField
-                                control={form.control}
-                                name="newPassword"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="text-sm">New Password</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="New Password"
-                                                type="password"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-
-                            <FormField
-                                control={form.control}
-                                name="confirmPassword"
-                                render={({ field }) => (
-                                    <FormItem>
-                                        <FormLabel className="text-sm">Confirm Password</FormLabel>
-                                        <FormControl>
-                                            <Input
-                                                placeholder="Confirm Password"
-                                                type="password"
-                                                {...field}
-                                            />
-                                        </FormControl>
-                                        <FormMessage />
-                                    </FormItem>
-                                )}
-                            />
-
-                            <Button
-                                type="submit"
-                                variant="outline"
-                                className="bg-[#FFAF8A] hover:bg-[#ffa880]"
-                            >
-                                Change Password
-                            </Button>
-                        </form>
-                    </Form>
-                </DialogContent>
-            </Dialog>
-        </>
-    );
+							<Button
+								type="submit"
+								variant="outline"
+								className="bg-[#FFAF8A] hover:bg-[#ffa880]"
+								disabled={mutation.isPending}
+							>
+								{mutation.isPending ? (
+									<>
+										<ReloadIcon className="mr-2 w-4 h-4 animate-pulse" />
+										Sending...
+									</>
+								) : (
+									"Send Reset Password Link"
+								)}
+							</Button>
+						</form>
+					</Form>
+				</DialogContent>
+			</Dialog>
+		</>
+	);
 };
 
 export default ForgotPasswordModal;
